6907f8eea32cdd7375b0ba64d39599f49da6ff7f
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-10-30 08:05:27 -0400

    asdf

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="pageContainer">
       <div className="homeHeader">
         <div className="imcLogo">
-           <h2>TOWN CENTER FITNESS</h2>
+            <img src={require("./img/TCF_CLEAN.svg")} />
           <div className="topTag">Bringing modern fitness to Southwood.</div>
         </div>
       </div>
