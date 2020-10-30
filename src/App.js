ceea34136a2ece8ce78ebe29a0b84413ad980d46
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-10-29 23:12:05 -0400

    framework layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import './App.scss';
+import { Switch, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Social from "./components/Social"
+
 
 function App() {
   return (
     <div className="App">
-      THisis towncenterfitnessapp
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+      <Social />
     </div>
   );
 }
---
+++ src/components/Social.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Social = () => {
+    return (
+        <div>
+            This is the Social Component
+        </div>
+    )
+}
+
+export default Social
