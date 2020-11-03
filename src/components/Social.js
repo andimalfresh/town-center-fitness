49d2f9a9de3ebc482a4e01bf147e6076d9621800
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-11-03 00:10:55 -0500

    hhh

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,110 +1,224 @@
-import React from "react";
-import { Button, Modal, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { Formik, Form, Field, setNestedObjectValues } from "formik";
+import * as Yup from "yup";
 import "../App.scss";
-import facebook from "./img/icons/001-facebook.svg"
-import twitter from "./img/icons/002-twitter.svg"
-import linkedIn from "./img/icons/010-linkedin.svg"
-import youTube from "./img/icons/008-youtube.svg"
+import facebook from "./img/icons/001-facebook.svg";
+import twitter from "./img/icons/002-twitter.svg";
+import linkedIn from "./img/icons/010-linkedin.svg";
+import youTube from "./img/icons/008-youtube.svg";
+import tcflogo from "./img/TCF_CLEAN.svg"
 
-function ModalForContact(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Connect with us Today.
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="modalDiv">
-        <Form>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Name" />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="email@example.com" />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control type="phone" placeholder="555-555-5555" />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows="3" />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button className="contactButton" onClick={props.onHide}>Send Info</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
+
+
+
 
 const Social = () => {
   const [modalShow, setModalShow] = React.useState(false);
+  const handleShowContact = () => {
+    setModalShow(true)
+  }
 
   return (
     <div className="socialDiv">
       <div className="modalButton">
-        <button id="connectWithUs" className="contactButton" onClick={() => setModalShow(true)}>
+        <button
+          id="connectWithUs"
+          className="contactButton"
+          onClick={() => handleShowContact}
+        >
           Contact Us Today
         </button>
         <div>Contact us today for more information.</div>
-        <ModalForContact show={modalShow} onHide={() => setModalShow(false)} />
+      
+                 {/* <Formik
+                 initialValues={{ name: "", email: "", phone: "", message: "" }}
+                 validate={(values) => {
+                   const errors = {};
+                   if (!values.email) {
+                     errors.email = "Required";
+                   } else if (
+                     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                   ) {
+                     errors.email = "Invalid email address";
+                   }
+                   return errors;
+                 }}
+                 onSubmit={(values, { setSubmitting }) => {
+                   setTimeout(() => {
+                     console.log(JSON.stringify(values, null, 2));
+                     setSubmitting(false);
+                   }, 400);
+                 }}
+               >
+                 {({
+                   values,
+                   errors,
+                   touched,
+                   handleChange,
+                   handleBlur,
+                   handleSubmit,
+                   isSubmitting,
+            
+                 }) => (
+                   <>
+                    
+                       <label>
+                         Connect with us Today.
+                       </label>
+               
+                     <div className="modalDiv">
+                       <Form onSubmit={handleSubmit}>
+                         <div>
+                           <label>Name</label>
+                           <Field
+                             type="name"
+                             name="name"
+                             onChange={handleChange}
+                             onBlur={handleBlur}
+                             value={values.name}
+                             placeholder="Name"
+                           />
+                         </div>
+                         <div>
+                           <label>Email address</label>
+                           <Field
+                             type="email"
+                             name="email"
+                             onChange={handleChange}
+                             onBlur={handleBlur}
+                             value={values.email}
+                             placeholder="email@example.com"
+                           />
+                         </div>
+                         <div>
+                           <label>Phone Number</label>
+                           <Field
+                             type="phone"
+                             name="phone"
+                             onChange={handleChange}
+                             onBlur={handleBlur}
+                             value={values.phone}
+                             placeholder="555-555-5555"
+                           />
+                         </div>
+                         <div>
+                           <label>Message</label>
+                           <Field
+                             as="textarea"
+                             type="message"
+                             name="message"
+                             onChange={handleChange}
+                             onBlur={handleBlur}
+                             value={values.message}
+                             rows="3"
+                           />
+                         </div>
+                       </Form>
+                     </div>
+                     <div>
+                       <button
+                         className="contactButton"
+                         type="submit"
+                         disabled={isSubmitting}
+                       >
+                         Send Info
+                       </button>
+                     </div>
+                   </>
+                 )}
+               </Formik>  */}
+
+<Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
+
       </div>
       <div className="socialWrapper">
         <div className="socialContact">
-          {/* <img
+          <img
             className="socialLogo"
-            src={require("./img/svg/imc-logo.svg")}
-          ></img> */}
+            src={tcflogo}
+          ></img>
           <div className="contactLinks">
             <div className="socialButton">
               <a href="/">HOME </a>
             </div>
             <div className="socialButton">
-              <a onClick={() => setModalShow(true)}>CONTACT</a>
+              <a>CONTACT</a>
             </div>
           </div>
           <div className="socialIcons">
             <a href="http://facebook.com">
-              <img
-                className="socialImg"
-                src={facebook}
-              />
+              <img className="socialImg" src={facebook} />
             </a>
             <a href="http://facebook.com">
-              <img
-                className="socialImg"
-                src={twitter}
-              />
+              <img className="socialImg" src={twitter} />
             </a>
             <a href="http://facebook.com">
               {" "}
-              <img
-                className="socialImg"
-                src={linkedIn}
-              />
+              <img className="socialImg" src={linkedIn} />
             </a>
             <a href="http://facebook.com">
               {" "}
-              <img
-                className="socialImg"
-                src={youTube}
-              />
+              <img className="socialImg" src={youTube} />
             </a>
           </div>
         </div>
         <div style={{ paddingBottom: "20px" }}>
           <div className="addressList">Town Center Fitness</div>
           <div className="addressList">
-          3196 Merchants Row Blvd, Tallahassee, FL, 32311 || Email: info@cospt.com{" "}
+            3196 Merchants Row Blvd, Tallahassee, FL, 32311
           </div>
         </div>
       </div>
