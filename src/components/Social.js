f3c8c85ac96e642656936e1ae1d7976c13c98102
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-11-03 21:31:32 -0500

    Nodemailer Works

--- src/components/Social.js
+++ src/components/Social.js
@@ -9,8 +9,10 @@ import linkedIn from "./img/icons/010-linkedin.svg";
 import youTube from "./img/icons/008-youtube.svg";
 import tcflogo from "./img/TCF_CLEAN.svg";
 
+const mailRoute = "https://tclnodemailer.herokuapp.com/send";
+
 const Social = () => {
-  const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow, setModalShow] = useState(false);
   const handleShowContact = () => {
     console.log("election Jesus");
     setModalShow(true);
@@ -28,92 +30,113 @@ const Social = () => {
         </button>
         <div>Contact us today for more information.</div>
         {modalShow ? (
-        <Formik
-          initialValues={{ name: "", email: "", phone: "", message: "" }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = "An Email is Required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <div>
-            <form onSubmit={handleSubmit} className="formikDiv">
-              <label className="fieldLabel">Name</label>
-              <input
-                type="name"
-                name="name"
-                placeholder="Name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.name}
-              />
-              <label className="errorLabel">{errors.message && touched.message && errors.message}</label>
-              <label className="fieldLabel">Email</label>
-              <input
-                placeholder="Your Email"
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              <label className="errorLabel">{errors.message && touched.message && errors.message}</label>
-              <label className="fieldLabel">Phone</label>
-              <input
-                placeholder="Phone #"
-                type="phone"
-                name="phone"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.phone}
-              />
-              <label className="errorLabel">{errors.message && touched.message && errors.message}</label>
-              <label className="fieldLabel">Message</label>
-              <textarea
-                placeholder="Type a message"
-                type="message"
-                name="message"
-                rows="4"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.message}
-              />
-              <label className="errorLabel">{errors.message && touched.message && errors.message}</label>
-              <div className="submitButtonDiv">
-                <button
-                  className="contactButton"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Submit
-                </button>
+          <Formik
+            initialValues={{ name: "", email: "", phone: "", message: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "An Email is Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                const sendPost = {
+                  name: values.name,
+                  email: values.email,
+                  phone: values.phone,
+                  message: values.message,
+                };
+                fetch(mailRoute, {
+                  method: "POST",
+                  body: JSON.stringify(sendPost),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }).then(window.location.reload());
+                setSubmitting(false)
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <div>
+                <form onSubmit={handleSubmit} className="formikDiv">
+                  <label className="fieldLabel">Name</label>
+                  <input
+                    type="name"
+                    name="name"
+                    placeholder="Name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                  />
+                  <label className="errorLabel">
+                    {errors.message && touched.message && errors.message}
+                  </label>
+                  <label className="fieldLabel">Email</label>
+                  <input
+                    placeholder="Your Email"
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <label className="errorLabel">
+                    {errors.message && touched.message && errors.message}
+                  </label>
+                  <label className="fieldLabel">Phone</label>
+                  <input
+                    placeholder="Phone #"
+                    type="phone"
+                    name="phone"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.phone}
+                  />
+                  <label className="errorLabel">
+                    {errors.message && touched.message && errors.message}
+                  </label>
+                  <label className="fieldLabel">Message</label>
+                  <textarea
+                    placeholder="Type a message"
+                    type="message"
+                    name="message"
+                    rows="4"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.message}
+                  />
+                  <label className="errorLabel">
+                    {errors.message && touched.message && errors.message}
+                  </label>
+                  <div className="submitButtonDiv">
+                    <button
+                      className="contactButton"
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
               </div>
-            </form>
-            </div>
-          )}
-        </Formik> ): null} 
+            )}
+          </Formik>
+        ) : null}
       </div>
       <div className="socialWrapper">
         <div className="socialContact">
