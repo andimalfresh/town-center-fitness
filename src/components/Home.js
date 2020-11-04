7c385b965ed05430c1fcfa7beee1fe603ab810df
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-11-04 08:07:41 -0500

    yoyoooo

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,8 @@ import introVid from "./img/videos/facebook_cover.mp4";
 import Social from "./Social";
 import { Player, LoadingSpinner } from "video-react";
 
-const vid = "http://media.w3.org/2010/05/bunny/trailer.mp4";
+const vid =
+  "https://drive.google.com/file/d/17rRX3J4M-HOmuycjXmqtlcfsrecJjXRN/preview";
 
 const Home = () => {
   return (
@@ -19,11 +20,11 @@ const Home = () => {
           <img src={gymLogo} className="tcfLogo" />
         </div>
         <div className="videoElement">
-        <p className="blueWaveText">
-          <h2>Your New neightborhood Fitness Center</h2>
+          <p className="blueWaveText">
+            <h2>Your New Southwood Fitness Center</h2>
             Town Center Fitness is excited to announce its grand Opening
-            December 5th ! Below you will find resources and information about
-            the developing changes and additions to your Southwood neightborhood
+            December 5th! Below you will find resources and information about
+            the developing changes and additions to your Southwood neighborhood
             fitness center. Feel free to reach out to us on social media or
             using the contact form below, and we will be happy to answer any
             questions you make have.
@@ -31,20 +32,16 @@ const Home = () => {
           <Player fluid autoPlay src={vid}>
             <LoadingSpinner />
           </Player>
-
         </div>
         <div className="contextElement">
           <img className="gymSvg" src={gym1} alt="test" />
 
           <p className="blueWaveText">
-            <h2>Lets Tell Whats Going On </h2>
-            Town Center Fitness is Southwood's newest modern gym working in
-            colloboration with MSK industry leaders JointStrong. The fitness
-            center offers 24 hour access to top of the line equiptment, weekly
-            classes to fit all scheules both virtual and in person as on site
-            MSK and mobility coaching. Stop by, give us a call or send us a
-            message and see how Town Center Fitness can help bring out the best
-            in the new you.
+            <h2>Pardon Our Dust</h2>
+            While are remaining open to our members 24/7, we are currently
+            making improvements for our December 5th, 2020 Grand Opening. We
+            cant wait to show you all the our new improvments and rennovations
+            to offer you the best neightborhood fitness experience.
           </p>
         </div>
         <div className="mockupContainer">
@@ -53,14 +50,14 @@ const Home = () => {
 
         <div className="contextElement">
           <p className="blueWaveText">
-          <h2>New Look, New Classes, New Support</h2>
+            <h2>New Look, New Classes, New Support</h2>
             Town Center Fitness is Southwood's newest modern gym working in
             colloboration with MSK industry leaders JointStrong. The fitness
-            center offers 24 hour access to top of the line equiptment, weekly
+            center offers 24 hour access, top of the line equiptment, weekly
             classes to fit all scheules both virtual and in person as on site
             MSK and mobility coaching. Stop by, give us a call or send us a
             message and see how Town Center Fitness can help bring out the best
-            in the new you.
+            in you.
           </p>
         </div>
 
@@ -72,13 +69,14 @@ const Home = () => {
 
         <div className="contextElement">
           <p className="blueWaveText">
-          <h2>We Would Love to Hear From You</h2>
+            <h2>We Would Love to Hear From You</h2>
             Our facility provides a full gym with weight machines, free weights,
             functional fitness equipment, spin classes and MSK coaching in the
             heart of Southwood. We practice current CDC guidelines for
             cleanileness and steralization ensuring our staff and guest stay
-            healthy and safe.
+            healthy and safe. 
           </p>
+          <p>We are open to feedback to deliver our guest and memebers the best possible experience. Feel free to contact us on any of our social media outlets or via the contact form below.</p>
         </div>
         <div className="mockupContainer">
           <img src={gymImage} className="mockupImage" />
